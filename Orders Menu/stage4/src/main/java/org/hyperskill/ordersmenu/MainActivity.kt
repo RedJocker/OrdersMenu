2f4ecbb9c1899ad1328a7275294b8dd58514ad0e
RedJocker <mauricio.guaruja@gmail.com>
2023-02-19 02:17:13 -0300

    stage4 solution

--- Orders Menu/stage4/src/main/java/org/hyperskill/ordersmenu/MainActivity.kt
+++ Orders Menu/stage4/src/main/java/org/hyperskill/ordersmenu/MainActivity.kt
@@ -12,11 +12,24 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import org.hyperskill.ordersmenu.theme.PlayOrdersMenuTheme
 
 class MainActivity : ComponentActivity() {
+
+    companion object {
+        private val recipesOnMenuToInitialStockMap = mapOf(
+            "Fettuccine" to 5,
+            "Risotto" to 6,
+            "Gnocchi" to 4,
+            "Spaghetti" to 3,
+            "Lasagna" to 5,
+            "Steak Parmigiana" to 2
+        )
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -27,7 +40,9 @@ class MainActivity : ComponentActivity() {
                 ) {
                     Column(Modifier.fillMaxSize()) {
                         Title(title = "Orders Menu")
-                        OrderMenuItem(itemName = "Fettuccine")
+                        recipesOnMenuToInitialStockMap.forEach { (recipeName, stockQuantity) ->
+                            OrderMenuItem(itemName = recipeName, stockQuantity = stockQuantity)
+                        }
                     }
                 }
             }
@@ -42,13 +57,13 @@ class MainActivity : ComponentActivity() {
     }
 
     @Composable
-    fun OrderMenuItem(itemName: String) {
+    fun OrderMenuItem(itemName: String, stockQuantity: Int) {
         Row(Modifier.fillMaxWidth(), Arrangement.Start, Alignment.Bottom) {
             var quantity by remember { mutableStateOf(0) }
 
             Text(
                 text = itemName,
-                color = if (quantity >= 5) Color.Red else Color.Black,
+                color = if (quantity >= stockQuantity) Color.Red else Color.Black,
                 fontSize = 24.sp
             )
 
@@ -58,14 +73,15 @@ class MainActivity : ComponentActivity() {
 //                                if(quantity == 1)
 //                                    quantity++
 
-                    if (quantity < 5) {
+                    if (quantity < stockQuantity) {
 //                                    quantity += 2
                         quantity++
 
                     }
-                })
+                }.testTag(itemName))
 
-            Text(text = "${quantity}", fontSize = 24.sp)
+
+            Text(text = "${quantity}", fontSize = 24.sp, modifier = Modifier.testTag(itemName))
 
             Text(text = "-", fontSize = 24.sp, modifier = Modifier
                 .padding(horizontal = 16.dp)

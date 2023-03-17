2d06aafa72d5b1b8ef27e0752256661abe449b7e
RedJocker <mauricio.guaruja@gmail.com>
2023-03-17 17:08:38 -0300

    removing solution from main branch

--- Orders Menu/stage1/src/main/java/org/hyperskill/ordersmenu/MainActivity.kt
+++ Orders Menu/stage1/src/main/java/org/hyperskill/ordersmenu/MainActivity.kt
@@ -6,7 +6,6 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.ui.Modifier
 import org.hyperskill.ordersmenu.theme.PlayOrdersMenuTheme
 
@@ -19,7 +18,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Text("Orders Menu")
+
                 }
             }
         }
